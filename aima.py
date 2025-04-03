4dad9bdf1879524a971a5aaa1b9134e0b621e9e8
abdulmumid <mitalipovabdulmumid@gmail.com>
2025-04-03 18:42:34 +0600

    abdulmumid

--- aima.py
+++ aima.py
@@ -6,3 +6,9 @@ class AimaMai():
     def info(self):
         return f' Менин баламдын аты {self.name} , жашы болсо{self.age}'
 
+class Abdulmumid(AimaMai):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+abdulmumid = Abdulmumid("Abdulmumid", 16)
+
