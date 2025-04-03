0855903f07e4ff0217e16ec0bd8b3b991b86b199
aiimvva <aiymtillaabdusalamova@gmail.com>
2025-04-03 18:24:21 +0600

    Initial commit

---
+++ aima.py
@@ -0,0 +1,8 @@
+class AimaMai():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        return f' Менин баламдын аты {self.name} , жашы болсо{self.age}'
+
